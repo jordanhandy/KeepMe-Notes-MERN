5fc46fb71ab28583f0ab6d72381b064987a87968
Jordan Handy <handy.jordan@gmail.com>
2020-02-23 10:26:43 -0500

    again

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ if (process.env.NODE_ENV === 'production'){
   app.use(express.static('client/build'));
 // Serve static files for initial run
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
 });
 }
 
